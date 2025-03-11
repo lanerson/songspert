f77687cd0dc2215068afc67e76d212763479ea60
lanerson <lanerson153@gmail.com>
2025-03-10 22:51:15 -0300

    criando projeto next

--- frontend/songspert/src/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-export default function NotFount(){
-    return(
-        <div>
-            <h2>Page not found</h2>
-            <p> Couldn't find requested resouce</p>
-            <a href="/">Home</a>
-        </div>
-    )
-}
