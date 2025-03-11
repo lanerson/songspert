fd5e0037e8b08ffc2b3cedb71e7ef4f37b0a3dbf
lanerson <lanerson153@gmail.com>
2025-03-10 22:37:23 -0300

    criando projeto next

---
+++ frontend/songspert/src/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+export default function Login(){
+    return (
+        <>
+            <text>Login</text>
+            <input type="text"/>
+            <text>Password</text>
+            <input type="password"/>
+        </>
+    );
+}
---
+++ frontend/songspert/src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFount(){
+    return(
+        <div>
+            <h2>Page not found</h2>
+            <p> Couldn't find requested resouce</p>
+            <a href="/">Home</a>
+        </div>
+    )
+}
