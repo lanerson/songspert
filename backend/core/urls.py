2a48add327146d95b72b1ecd84bdff4d2ed27eef
Schuinki <rafaelschuinki@outlook.com>
2025-03-12 13:02:39 -0300

    API Deezer no Django

---
+++ backend/core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import index, search_track, track_details
+
+urlpatterns = [
+	path('', index),
+    path("search/", search_track, name="search_track"),
+    path("track/<int:track_id>/", track_details, name="track_details"),
+]
--- backend/songspert/settings.py
+++ backend/songspert/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -125,3 +125,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEEZER_API_URL = "https://api.deezer.com"
